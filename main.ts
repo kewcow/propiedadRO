e95da1c19118274ad956de73edf760747880deec
kewcow <kevjhoss@gmail.com>
2022-11-24 20:09:47 -0300

    version 1.2

--- main.ts
+++ main.ts
@@ -5,6 +5,9 @@ import {home} from './router.ts';
 const handler = async (req: Request): Promise<Response> => {
   const pathname = new URL(req.url).pathname;
   if (pathname === '/') return home();
+  if (pathname === '/emprendimientos') return home();
+  if (pathname === '/departamentos') return home();
+  if (pathname === '/contacto') return home();
 
   const css = await Deno.readFile(`${Deno.cwd()}/dist${pathname}`);
   if (pathname.split('.').at(-1) === 'css') return new Response(css, headers('css'));
--- router.ts
+++ router.ts
@@ -2,5 +2,5 @@ import {headers} from './header.ts';
 
 export const home = async (): Promise<Response> => {
   const file = await Deno.readFile(`./dist/index.html`);
-  return new Response(file, headers())
+  return new Response(file, headers('html'));
 }
